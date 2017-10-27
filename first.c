468ab0a67c9e226c6bed7252450bcab2558a7f5f
jia-hao-liu <32381123+jia-hao-liu@users.noreply.github.com>
2017-10-27 16:39:17 +0800

    Initial commit

---
+++ first.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void report_count();
+char choice(void);
+int count = 0;
+int at(int a )
+{
+    int value;
+    register int i;
+    i=0;
+    printf("hahahah:");
+
+    return 0;
+}
+
+
+
+
+
+
---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int at(int );
+int main()
+{
+    printf("Hello world!\n");
+
+    at(0);
+    return 0;
+
+}
